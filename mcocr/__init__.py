8085ca222d6d3d398e25ca89f01ca181b4317417
Barney Gale <barney@signal.(none)>
2012-03-29 09:03:18 +0100

    Tabs -> Spaces

--- mcocr/__init__.py
+++ mcocr/__init__.py
@@ -3,8 +3,8 @@ import screenshot
 import sys
 
 if __name__ == '__main__':
-	f = font.Font()
-	s = screenshot.Screenshot(sys.argv[1], f)
-	print s.text
-	#print s.colours
-	#print s.get_coords_positions()
+    f = font.Font()
+    s = screenshot.Screenshot(sys.argv[1], f)
+    print s.text
+    #print s.colours
+    #print s.get_coords_positions()
